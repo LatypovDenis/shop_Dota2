f484b538649349f88e2fe5a84541e8d0cbf7bb24
Denis <dnslatypov@gmail.com>
2025-05-25 15:39:24 +0500

    1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 // import { ProductItem } from './productList/Productitem/Productitem';
 import { ProductList } from './productList/ProductList';
